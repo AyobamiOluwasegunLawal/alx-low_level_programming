d15cb2fb709e5fcb68ff17dc6dc990c7671e0a0b
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2022-09-09 08:22:41 +0000

    another commited message.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase with propergrammar,
- * but the outcome is a piece of art,
+ * main - Prints out an output
+ *
  * Return: 0 if exited properly, non-zero otherwise
+ *
+ * Description: Prints out an output
  */
 
 int main(void)
