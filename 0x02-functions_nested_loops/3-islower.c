659116b865fb2c3ac14a520371050bc820ca8287
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2022-09-14 14:46:56 +0100

    THis is another hot fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,8 +3,11 @@
 /**
   * _islower - checks if letter are lowercase
   *
-  * Returns: 1 or 0 depending on the outcome of condition
+  * @c: this is a function hat holds (int c) as a parameter
+  *
+  * Return: 1 or 0 depending on the outcome of condition
   */
+
 int _islower(int c)
 {
 	return (c >= 'a' && c <= 'z');
