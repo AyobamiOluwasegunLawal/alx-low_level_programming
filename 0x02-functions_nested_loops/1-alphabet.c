ad293ce8925038b821f741f4263e64e2591fe2a0
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2022-09-14 10:19:38 +0100

    this is my second commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,2 +1,22 @@
 #include "main.h"
 
+/**
+  *print_alphabet - Entry point
+  *
+  *Description: This calls a function that prints out letter form a-z
+  *
+  *Return: (0) when successful
+  */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
+
