3b46a302f68626cfc0738c213f404924978c7d24
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2022-09-14 14:01:20 +0100

    This returns 0 or 1 depending on the outcome of the conditions

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * _islower - checks if letter are lowercase
+  *
+  * Returns: 1 or 0 depending on the outcome of condition
+  */
+int _islower(int c)
+{
+	return (c = 'a' && c <= 'z');
+}
