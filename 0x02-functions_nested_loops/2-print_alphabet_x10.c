3531cc8f52e32ba56ccdfff62b61763bacfe9344
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2022-09-14 11:32:35 +0100

    this is another commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  *print_alphabet_x10 - Entry point
+  *
+  */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+
+	}
+}
+
