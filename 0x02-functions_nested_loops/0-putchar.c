8e29ce15dc89f04548ee6caea2ab739abf786828
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2022-09-14 05:22:07 +0100

    this is my first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,22 +3,22 @@
 /**
   *main - Entry point
   *
-  *Description: To print out a text
+  *Description: Prints -putchar
   *
-  *Return:0 when sucessful
+  *Return: 0 when successful
   */
 
 int main(void)
 {
-   putchar('_');
-   putchar('p');
-   putchar('u');
-   putchar('t');
-   putchar('c');
-   putchar('h');
-   putchar('a');
-   putchar('r');
-   putchar('\n');
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 
 	return (0);
 }
