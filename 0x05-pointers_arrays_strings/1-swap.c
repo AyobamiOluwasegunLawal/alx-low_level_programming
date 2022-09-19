fc6a481f5bc226d5a7afc197b51e47209d99d6c5
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2022-09-19 17:49:30 +0100

    this is project 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * swap_int - a function call
+  *
+  * @a: parameter 1
+  * @b: parameter 2
+  *
+  * Description: this function swaps integers
+  */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
