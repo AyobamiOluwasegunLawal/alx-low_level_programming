3684d98febc45f6a83b4d8fb2d6b10a35a63f3f9
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2022-09-21 06:02:26 +0100

    this is task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * print_rev - function call from main
+  * @s: Parameter 1
+  *
+  * Return: void
+  */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s) - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
