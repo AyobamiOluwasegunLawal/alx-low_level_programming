e739f0a6115dc5c0b4655d8e852d029cca76d49a
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2022-09-21 04:54:46 +0100

    this is task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,14 @@
 #include "main.h"
-
-
+#include <string.h>
+/**
+  * strlen - this gives the length of the string
+  * @s: parameter 1
+  *
+  * Description: gives the length of a string
+  *
+  * Returns: length of string
+  */
 int _strlen(char *s)
 {
-	
+	return (strlen(s));
 }
