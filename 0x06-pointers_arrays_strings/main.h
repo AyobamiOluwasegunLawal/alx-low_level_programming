8dc08866189caeb6092fa30fd5c0a0503187463b
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2022-09-24 02:38:07 +0100

    this is my second commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H_
 #define MAIN_H_
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *_strcat(char *dest, char *src);
 int _putchar(char c);
 char *_strcat(char *, char *);
