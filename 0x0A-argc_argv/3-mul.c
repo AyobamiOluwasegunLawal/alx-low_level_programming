52d8d0f09e7277ae06cb2cd225757fb28c3070e5
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2022-10-03 20:10:48 +0100

    this is task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+/**
+  * main - Entry point
+  * @argc: parameter 1
+  * @argv: parameter 2
+  *
+  * Return: 0 when successful
+  */
+
+int main(int argc, char *argv[])
+{
+	int result = 0, num1, num2;
+	
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
+}
+
