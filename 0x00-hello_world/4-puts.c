5630f75750247aa66d668ade1a18afe2efe0f2ca
Ayobami Oluwaseegun Lawal <ayobamioluwasegslawal@gmail.com>
2023-12-15 02:07:06 +0000

    output a string using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints out an output using put
+ *
+ * Return: always (0) on success
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
