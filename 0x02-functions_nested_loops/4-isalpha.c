2b15fac2ec1d01c8907111c8c3dec23157e27e4c
Ayobami Oluwaseegun Lawal <ayobamioluwasegslawal@gmail.com>
2023-12-19 19:10:09 +0000

    check for alphabetic characters and also added the function prototype into the main.h file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * isalpha - checks for alphabetic character
+ * @c: character to check
+ *
+ * Return: (1) when successful
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
