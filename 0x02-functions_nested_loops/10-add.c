662cf8f637a3fd6ce0d9d4dd0579ab8cf4e81765
Ayobami Oluwaseegun Lawal <ayobamioluwasegslawal@gmail.com>
2023-12-20 21:38:59 +0000

    funtion to add two integers and also added the function prototype in the main.h file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Add two integers
+ *@a: first integer parameter
+ *@b: second integer parameter
+ *
+ * Return: the result of the sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
