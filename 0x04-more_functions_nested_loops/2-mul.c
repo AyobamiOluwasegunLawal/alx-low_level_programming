826d04cc0cec1a4f1c35ff42860d3f6b713c347b
Ayobami Oluwaseegun Lawal <ayobamioluwasegslawal@gmail.com>
2023-12-21 16:20:10 +0000

    multiplys two integers, function prototype in the .h file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: takes the first integer
+ * @b: takes the second integer
+ *
+ * Return: result of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
