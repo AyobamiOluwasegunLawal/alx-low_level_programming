bf2210fee666f7906ad45a32873595b7b9a1920f
Ayobami Oluwaseegun Lawal <ayobamioluwasegslawal@gmail.com>
2023-12-21 19:17:52 +0000

    print dashes, function prototype is in .h file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print the \ diagonally
+ * @n: nober of \ to print
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n == 0 || n < 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= n; j++)
+			{
+				if (j >= n - (n - i) && j <= i)
+				{
+					_putchar('\\');
+				} else
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
