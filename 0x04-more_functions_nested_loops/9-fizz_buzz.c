4128f1a5d7ff151918a6c964168ba406b31bd172
Ayobami Oluwaseegun Lawal <ayobamioluwasegslawal@gmail.com>
2023-12-22 18:21:59 +0000

    modified the code

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,15 +11,18 @@ int main(void)
 
 	while (n <= 100)
 	{
-		if (n % 3 == 0 && n % 5 == 0)
+		if ((n % 3 == 0) && (n % 5 == 0))
 		{
 			printf("FizzBuzz ");
-		} else if (n % 3 == 0)
+		} else if ((n % 3 == 0))
 		{
 			printf("Fizz ");
-		} else if (n % 5 == 0)
+		} else if ((n % 5 == 0))
 		{
-			printf("Buzz ");
+			if (n == 100)
+				printf("Buzz");
+			else
+				printf("Buzz ");
 		} else
 		{
 			printf("%d ", n);
