46e81ec27d004fe3b7232725fc6240233c5efbb5
Ayobami Oluwaseegun Lawal <ayobamioluwasegslawal@gmail.com>
2024-01-02 10:42:49 +0000

    print strings in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse order
+ * @s: the input string
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	char *reset = s;
+
+	while (*s != '\0')
+	{
+		len += 1;
+		s += 1;
+	}
+
+	while (s >= reset)
+	{
+		_putchar(*s);
+		--s;
+	}
+	_putchar('\n');
+}
