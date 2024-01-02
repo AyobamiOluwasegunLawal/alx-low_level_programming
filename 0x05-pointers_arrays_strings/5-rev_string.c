c116c8c88f9761cff197df0c8da7fc770b24b310
Ayobami Oluwaseegun Lawal <ayobamioluwasegslawal@gmail.com>
2024-01-02 13:38:36 +0000

    prints integers in reverse order

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses string
+ * @c: input pointer to string
+ */
+void rev_string(char *c)
+{
+	int i = 0, len =  0;
+	char temp;
+
+	while (s[i++])
+		len++;
+
+	for (i = len - 1; i >= len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
