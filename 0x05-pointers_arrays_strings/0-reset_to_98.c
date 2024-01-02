e9b0cd9853e83c8edbbc791861cdcd750b4e14eb
Ayobami Oluwaseegun Lawal <ayobamioluwasegslawal@gmail.com>
2024-01-02 06:45:23 +0000

    Contain funtion to change an int value and a readme file about the task with the required prototypes in the .h file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Changes integers to another value (98)
+ * @n: Expects an address
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+1. Made a c file that contains the function to turn an integer value to another integer value 98
