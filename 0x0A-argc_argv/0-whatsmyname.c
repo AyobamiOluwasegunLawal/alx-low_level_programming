8ed3f3001c7de5544fc6c3b2508a00e774a1a6cd
Ayobami Oluwaseegun Lawal <ayobamioluwasegslawal@gmail.com>
2024-01-12 09:14:29 +0000

    print out it's name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints it's name
+ * @argc: count
+ * @argv: pointer to string input
+ *
+ * Return: (0) when successful
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Project on argument to main.
