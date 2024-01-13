78643241b5ec313343e4c7b54f8a77c5d26e35a8
Ayobami Oluwaseegun Lawal <ayobamioluwasegslawal@gmail.com>
2024-01-13 08:15:59 +0000

    modified the code

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,13 +12,16 @@ int main(int argc, char *argv[])
 {
 	int mul = 1, i;
 
-	if (argc > 1)
+	if ((argc >= 1 && argc <= 2) || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
 	{
 		for (i = 1; i < argc; i++)
 			mul *= atoi(argv[i]);
-		printf("%d\n", mul);
 	}
-	else
-		printf("Error\n");
+	printf("%d\n", mul);
 	return (0);
 }
