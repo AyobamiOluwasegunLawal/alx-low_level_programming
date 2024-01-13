2254e0b0d2304a00171d5058170493592b4a1bf8
Ayobami Oluwaseegun Lawal <ayobamioluwasegslawal@gmail.com>
2024-01-13 07:36:48 +0000

    made a little modification

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - add positive numbers
+ * @argc: count input argument
+ * @argv: pointer to argument string
+ *
+ * Return: (0) when successful
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (!(isdigit(argv[i][j])))
+				{
+					printf("Error\n");
+					return (1);
+				}
+				sum += atoi(argv[i]);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("%d\n", 0);
+	}
+	return (0);
+}
