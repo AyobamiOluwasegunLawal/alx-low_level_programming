24c5e098d515ef2169f5f378675dbb6782b60245
Ayobami Oluwaseegun Lawal <ayobamioluwasegslawal@gmail.com>
2024-01-23 03:14:01 +0000

    task 0

---
+++ 0x0E-structures_typedef/RAEDME.md
@@ -0,0 +1,1 @@
+Contains code to all task of project 0x0E. C - Structures, typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure of name dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: this structure is for dogs
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
