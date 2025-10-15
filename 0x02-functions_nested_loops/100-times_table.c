e75f4731e002db3338a28a23f7d47fc5099faaf4
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2025-10-15 18:52:55 +0100

    adjust betty funtion length

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,21 +7,15 @@
 
 void print_times_table(int n)
 {
-	int i, j;
-	int mul;
-	int temp, div, digit, num;
+	int i, j, mul, temp, div, digit, num;
 
 	if (n > 15 || n < 0)
-	{
 		return;
-	}
-
 	for (i = 0; i <= n; i++)
 	{
-		for(j = 0; j <= n; j++) 
+		for (j = 0; j <= n; j++)
 		{
 			mul = i * j;
-			
 			if (j != 0)
 			{
 				_putchar(',');
@@ -33,31 +27,24 @@ void print_times_table(int n)
 					_putchar(' ');
 				}
 				else if (mul < 100)
-				{
 					_putchar(' ');
-				}
 			}
-
 			temp = mul;
 			div = 1;
-
 			while (temp > 9)
 			{
 				temp /= 10;
 				div *= 10;
 			}
-			
 			num  = mul;
-
 			while (div > 0)
 			{
 				digit = num / div;
-				_putchar(digit +'0');
+				_putchar(digit + '0');
 				num %= div;
 				div /= 10;
 			}
 		}
-
 		_putchar('\n');
 	}
 }
