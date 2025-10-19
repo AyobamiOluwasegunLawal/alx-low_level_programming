f929ddaa9ee51d945db8bca81a4d67cc94d8f72e
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2025-10-19 20:04:32 +0100

    made another adjust

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,11 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0, j = 0;
+	int i = 0;
 
-	while (src[i] != '\0' && j < n)
+	while (src[i] != '\0' && i < n)
 	{
-		dest[i] = src[j];
-		j++;
+		dest[i] = src[i];
 		i++;
 	}
 
